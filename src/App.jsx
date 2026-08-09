cfa8fe30081585c65a792065f4d4ec0b4d51cb7d
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-08-08 23:23:02 -0300

    feat: allow logged in users to create new blogs

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -54,6 +57,22 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreateBlog = async event => {
+    event.preventDefault()
+
+    const newBlog = {
+      title: title,
+      author: author,
+      url: url
+    }
+    const savedBlog = await blogService.create(newBlog)
+    setBlogs(blog => blog.concat(savedBlog))
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -78,6 +97,36 @@ const App = () => {
     </form>
   )
 
+  const blogForm = () => (
+    <form onSubmit={handleCreateBlog}>
+      <div>
+        <label>
+          title
+          <input type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          author
+          <input type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          url
+          <input type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)} />
+        </label>
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
@@ -86,7 +135,8 @@ const App = () => {
       {!user && loginForm()}
       {user && (
         <div>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in</p>          
+          {blogForm()}
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
