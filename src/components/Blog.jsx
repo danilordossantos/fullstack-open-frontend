0534792494a93cb8ca0175e85c18eaca8e127e49
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-08-16 00:26:14 -0300

    feat: add view/hide toggle for blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      {blog.title} {blog.author}
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}</div>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
